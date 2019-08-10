f2f1124e140b511981da6f6d50ff0ed75d723b20
Florian Lammel <florian.lammel@gesagt-getan.at>
2019-08-10 17:27:30 +0200

    Fix bug causing disappearing foods and recipes

--- src/Foods/FoodsRepository.ts
+++ src/Foods/FoodsRepository.ts
@@ -1,14 +1,20 @@
 import { Food, FoodId } from "./Data";
 
-function load(): Food[] {
+function loadIncludingDeleted(): Food[] {
     const items: Food[] = JSON.parse(window.localStorage.getItem("foods")) || [];
-    return items.filter((i) => !i.next).filter((i) => !i.isDeleted);
+    return items;
+}
+
+function load(): Food[] {
+    return loadIncludingDeleted()
+        .filter((i) => !i.next)
+        .filter((i) => !i.isDeleted);
 }
 
 function create(newFoodData: Food) {
     const id = Math.floor(Math.random() * 1000000);
     const newFood = { ...newFoodData, id };
-    window.localStorage.setItem("foods", JSON.stringify([...load(), newFood]));
+    window.localStorage.setItem("foods", JSON.stringify([...loadIncludingDeleted(), newFood]));
 }
 
 function update(food: Food) {
@@ -16,26 +22,29 @@ function update(food: Food) {
     const newFood = { ...food, id };
     window.localStorage.setItem(
         "foods",
-        JSON.stringify([...load().map((i) => (i.id === food.id ? { ...i, next: newFood.id } : i)), newFood]),
+        JSON.stringify([
+            ...loadIncludingDeleted().map((i) => (i.id === food.id ? { ...i, next: newFood.id } : i)),
+            newFood,
+        ]),
     );
 }
 
 function remove(food: Food) {
     window.localStorage.setItem(
         "foods",
-        JSON.stringify(load().map((i) => (i.id === food.id ? { ...food, isDeleted: true } : i))),
+        JSON.stringify(loadIncludingDeleted().map((i) => (i.id === food.id ? { ...food, isDeleted: true } : i))),
     );
 }
 
 function undoDelete(food: Food) {
     window.localStorage.setItem(
         "foods",
-        JSON.stringify(load().map((i) => (i.id === food.id ? { ...food, isDeleted: false } : i))),
+        JSON.stringify(loadIncludingDeleted().map((i) => (i.id === food.id ? { ...food, isDeleted: false } : i))),
     );
 }
 
 function byId(id: FoodId): Food | null {
-    for (const food of load()) {
+    for (const food of loadIncludingDeleted()) {
         if (food.id === id) {
             return food;
         }
--- src/Recipes/RecipesRepository.ts
+++ src/Recipes/RecipesRepository.ts
@@ -1,14 +1,18 @@
 import { Recipe, RecipeId } from "./Data";
 
-function load(): Recipe[] {
+function loadIncludingDeleted(): Recipe[] {
     const items: Recipe[] = JSON.parse(window.localStorage.getItem("recipes")) || [];
-    return items.filter((i) => !i.isDeleted);
+    return items;
+}
+
+function load(): Recipe[] {
+    return loadIncludingDeleted().filter((i) => !i.isDeleted);
 }
 
 function create(newRecipeData: Recipe) {
     const id = Math.floor(Math.random() * 1000000);
     const newRecipe = { ...newRecipeData, id };
-    window.localStorage.setItem("recipes", JSON.stringify([...load(), newRecipe]));
+    window.localStorage.setItem("recipes", JSON.stringify([...loadIncludingDeleted(), newRecipe]));
 }
 
 function update(recipe: Recipe) {
@@ -16,26 +20,29 @@ function update(recipe: Recipe) {
     const newRecipe = { ...recipe, id };
     window.localStorage.setItem(
         "recipes",
-        JSON.stringify([...load().map((i) => (i.id === recipe.id ? { ...i, next: newRecipe.id } : i)), newRecipe]),
+        JSON.stringify([
+            ...loadIncludingDeleted().map((i) => (i.id === recipe.id ? { ...i, next: newRecipe.id } : i)),
+            newRecipe,
+        ]),
     );
 }
 
 function remove(recipe: Recipe) {
     window.localStorage.setItem(
         "recipes",
-        JSON.stringify(load().map((i) => (i.id === recipe.id ? { ...recipe, isDeleted: true } : i))),
+        JSON.stringify(loadIncludingDeleted().map((i) => (i.id === recipe.id ? { ...recipe, isDeleted: true } : i))),
     );
 }
 
 function undoDelete(recipe: Recipe) {
     window.localStorage.setItem(
         "recipes",
-        JSON.stringify(load().map((i) => (i.id === recipe.id ? { ...recipe, isDeleted: false } : i))),
+        JSON.stringify(loadIncludingDeleted().map((i) => (i.id === recipe.id ? { ...recipe, isDeleted: false } : i))),
     );
 }
 
 function byId(id: RecipeId): Recipe | null {
-    for (const recipe of load()) {
+    for (const recipe of loadIncludingDeleted()) {
         if (recipe.id === id) {
             return recipe;
         }
