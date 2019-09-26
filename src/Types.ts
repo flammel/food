7f9d361d5e5cdb9fca901e2cbcacdbe6e260ecd9
Florian Lammel <florian.lammel@gesagt-getan.at>
2019-09-26 21:20:02 +0200

    Handle NaN when formatting nutrition values and calories

--- src/Types.ts
+++ src/Types.ts
@@ -18,11 +18,11 @@ export function formatQuantity(quantity: Quantity): string {
 }
 
 export function formatCalories(calories: Calories): string {
-    return calories.toFixed(0);
+    return isNaN(calories) ? "0" : calories.toFixed(0);
 }
 
 export function formatNutritionValue(value: NutritionValue): string {
-    return value.toFixed(1);
+    return isNaN(value) ? "0" : value.toFixed(1);
 }
 
 export function isUnit(value: string): value is Unit {
